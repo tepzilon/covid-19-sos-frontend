2d3eaeb9067bbc6975cf195aa9cc8ed47ca65b46
techid2000 <techid2000@gmail.com>
2020-04-03 02:19:29 +0700

    fix(requests): minor fix

--- store/requests/requests.utils.js
+++ store/requests/requests.utils.js
@@ -4,6 +4,6 @@ export const getFilteredRequests = (requests, filter) => {
   return requests.filter(
     request =>
       request[requestField.hospitalName].includes(filter) ||
-      request[requestField.address].includes(filter)
+      request[requestField.province].includes(filter)
   );
 };
