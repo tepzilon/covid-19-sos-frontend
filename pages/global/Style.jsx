8e5d595bce53b0d4781ae7bf048f558c049160c0
Chavin Chuangchaichatchavarn <cc6chavin@gmail.com>
2020-04-02 23:48:35 +0700

    refactor(app): move global header & style to render on _app

---
+++ pages/global/Style.jsx
@@ -0,0 +1,17 @@
+const Style = () => (
+  <style jsx global>{`
+    html,
+    body {
+      padding: 0;
+      margin: 0;
+      font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+        Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+    }
+
+    * {
+      box-sizing: border-box;
+    }
+  `}</style>
+);
+
+export default Style;
