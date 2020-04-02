e7a7ff82ed5e04a3a8b67aeb5bd0037b7c324050
Chavin Chuangchaichatchavarn <cc6chavin@gmail.com>
2020-04-02 21:37:42 +0700

    feat(register): create dummy register abstraction
    change will be made to match backend schema

---
+++ pages/register.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { inputField, hospitalTypes } from '../types';
+
+const Register = () => {
+  return (
+    <div>
+      <input type="text" placeholder={inputField.name} />
+    </div>
+  );
+};
+
+export default Register;
---
+++ types/register.js
@@ -0,0 +1,16 @@
+export const inputField = {
+  name: 'name',
+  hospitalName: 'hospitalName',
+  faceShieldDemand: 'faceShieldDemand',
+  address: 'address',
+  hospitalType: 'hospitalType',
+  numberOfBed: 'numberOfBed',
+  notice: 'notice',
+  id: 'id',
+  password: 'password'
+};
+
+export const hospitalTypes = {
+  public: 'public',
+  private: 'private'
+};
