0a2361fbaebea1e63c70efe6d142d9ed2548edb9
techid2000 <techid2000@gmail.com>
2020-04-03 16:40:56 +0700

    fix(store): minor fix for test

--- store/store.js
+++ store/store.js
@@ -6,17 +6,29 @@ import * as requestsReducer from './requests/requests.reducer';
 
 const StoreContext = createContext({});
 
-const getReducerObject = ({ reducer, initialState }) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-  return { state, dispatch };
-};
+// const getReducerObject = ({ reducer, initialState }) => {
+//   const [state, dispatch] = useReducer(reducer, initialState);
+//   return { state, dispatch };
+// };
 
 export const StoreProvider = ({ children }) => {
+  // const store = {
+  //   counter: getReducerObject(counterReducer),
+  //   register: getReducerObject(registerReducer),
+  //   requests: getReducerObject(requestsReducer)
+  // };
+
+  const [counterState, counterDispatch] = useReducer(counterReducer.reducer, counterReducer.initialState);
+  const [registerState, registerDispatch] = useReducer(counterReducer.reducer, counterReducer.initialState);
+  const [requestsState, requestsDispatch] = useReducer(counterReducer.reducer, counterReducer.initialState);
+  
   const store = {
-    counter: getReducerObject(counterReducer),
-    register: getReducerObject(registerReducer),
-    requests: getReducerObject(requestsReducer)
+    counter: {state: counterState, dispatch: counterDispatch},
+    register: {state: registerState, dispatch: registerDispatch},
+    requests: {state: requestsState, dispatch: requestsDispatch}
   };
+
+
   return (
     <StoreContext.Provider value={store}>{children}</StoreContext.Provider>
   );
