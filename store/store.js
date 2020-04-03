d57b5b39fd8f11cb4f9597ddc7025eb3a1c4801e
techid2000 <techid2000@gmail.com>
2020-04-03 17:07:33 +0700

    fix(store): undo

--- store/store.js
+++ store/store.js
@@ -6,29 +6,26 @@ import * as requestsReducer from './requests/requests.reducer';
 
 const StoreContext = createContext({});
 
-// const getReducerObject = ({ reducer, initialState }) => {
-//   const [state, dispatch] = useReducer(reducer, initialState);
-//   return { state, dispatch };
-// };
+const getReducerObject = ({ reducer, initialState }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return { state, dispatch };
+};
 
 export const StoreProvider = ({ children }) => {
-  // const store = {
-  //   counter: getReducerObject(counterReducer),
-  //   register: getReducerObject(registerReducer),
-  //   requests: getReducerObject(requestsReducer)
-  // };
-
-  const [counterState, counterDispatch] = useReducer(counterReducer.reducer, counterReducer.initialState);
-  const [registerState, registerDispatch] = useReducer(counterReducer.reducer, counterReducer.initialState);
-  const [requestsState, requestsDispatch] = useReducer(counterReducer.reducer, counterReducer.initialState);
-  
   const store = {
-    counter: {state: counterState, dispatch: counterDispatch},
-    register: {state: registerState, dispatch: registerDispatch},
-    requests: {state: requestsState, dispatch: requestsDispatch}
+    counter: getReducerObject(counterReducer),
+    register: getReducerObject(registerReducer),
+    requests: getReducerObject(requestsReducer)
   };
-
-
+  // const [counterState, counterDispatch] = useReducer(counterReducer.reducer, counterReducer.initialState);
+  // const [registerState, registerDispatch] = useReducer(counterReducer.reducer, counterReducer.initialState);
+  // const [requestsState, requestsDispatch] = useReducer(counterReducer.reducer, counterReducer.initialState);
+  
+  // const store = {
+  //   counter: {state: counterState, dispatch: counterDispatch},
+  //   register: {state: registerState, dispatch: registerDispatch},
+  //   requests: {state: requestsState, dispatch: requestsDispatch}
+  // };
   return (
     <StoreContext.Provider value={store}>{children}</StoreContext.Provider>
   );
