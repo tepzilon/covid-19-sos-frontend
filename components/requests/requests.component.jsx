5b5dabbc9992bad12e2b4971b1853ab6f66a6f7e
Nattapat <mark10001971@hotmail.com>
2020-04-03 23:51:26 +0700

    feat(requests): add modal

--- components/requests/requests.component.jsx
+++ components/requests/requests.component.jsx
@@ -1,29 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { StoreConnect } from "../../store/store";
 import styles from "./requests.module.scss";
 import RequestsTable from './requestsTable/requestsTable.component'
-import { Card, Select } from 'antd'
+import { Card, Select, Modal } from 'antd'
 import { updateFilter } from '../../store/requests/requests.actions';
 import { requestField } from '../../types';
 
 const { Option } = Select;
 
 const Requests = ({updateFilter, requestsValue}) => {
+    const [showModal, setShowModal] = useState(false);
+    const [currentData, setCurrentData] = useState(null);
 
     return (
         <Card title={<h1 style={{textAlign: "center"}}>รายชื่อโรงพยาบาลที่ลงทะเบียนเพื่อขอรับ Face Shield</h1>}>
             <div style={{margin: 'auto', width: '50%'}}>
                 <p style={{display: 'inline'}} >ค้นหา</p>
-                <Select showSearch placeholder="ชื่อโรงพยาบาล/จังหวัด" style={{width: '80%'}} onChange={e => updateFilter(e.target.value)} >
+                <Select 
+                    showSearch 
+                    optionFilterProp="children"
+                    filterOption={(input, option) =>
+                        option.props.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+                    }  
+                    placeholder="ชื่อโรงพยาบาล/จังหวัด" 
+                    style={{width: '80%'}} 
+                >
                     {
-                        requestsValue.map(e => {
+                        requestsValue.map((e,i) => {
                             return (
-                                <Option value={e[requestField.hospitalName]}>{e[requestField.hospitalName]}</Option>
+                                <Option value={e[requestField.hospitalName]} key={i} >{e[requestField.hospitalName]}</Option>
                             );
                         })
                     }
                 </Select>
             </div>
+            <Modal
+                title="Modal"
+                visible={showModal}
+            >
+                Hello
+            </Modal>
             <RequestsTable />
         </Card>
     );
