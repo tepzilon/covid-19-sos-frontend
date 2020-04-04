b6ef360917c04b66ba9af204e7586c07ebb5f1bd
Chavin Chuangchaichatchavarn <cc6chavin@gmail.com>
2020-04-05 04:08:53 +0700

    feat(pageHeader): implement pageHeader
    add logo to pageHeader and style it'

--- components/pageHeader/pageHeader.component.jsx
+++ components/pageHeader/pageHeader.component.jsx
@@ -1,3 +1,28 @@
-const PageHeader = ({ label }) => <h1>{label}</h1>;
+const PageHeader = ({ label }) => (
+  <>
+    <div className="container-center">
+      <img
+        src="cu_eng_meticuly.png"
+        alt="chula engineering and meticuly logo"
+      />
+    </div>
+    <h1>{label}</h1>
+    <style jsx>
+      {`
+        img {
+          width: 120px;
+          height: 120px;
+          margin-bottom: 24px;
+        }
+        h1 {
+          text-align: center;
+          font-weight: bold;
+          font-size: 29px;
+          line-height: 40px;
+        }
+      `}
+    </style>
+  </>
+);
 
 export default PageHeader;
