2d57192d8e5014997965c74af6251728e7304d2f
techid2000 <techid2000@gmail.com>
2020-04-04 11:27:12 +0700

    feat(app: added meta

--- pages/global/Header.jsx
+++ pages/global/Header.jsx
@@ -4,6 +4,7 @@ const Header = () => (
   <Head>
     <title>Face Shield</title>
     <link rel="icon" href="/favicon.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
   </Head>
 );
 
