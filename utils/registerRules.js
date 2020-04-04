95facbc48be035aee7da979ba69472770b18849e
Chavin Chuangchaichatchavarn <cc6chavin@gmail.com>
2020-04-05 04:09:25 +0700

    feat(register): change wording to match design

--- utils/registerRules.js
+++ utils/registerRules.js
@@ -1,6 +1,6 @@
 export const RequiredFieldRule = (text) => ({
   required: true,
-  message: `กรุณากรอก${text}`,
+  message: `กรุณากรอก${text.match(/^[A-Za-z]/) ? ' ' : ''}${text}`,
 });
 export const phoneRule = () => ({
   pattern: new RegExp(/^0[0-9]-[0-9]{4}-[0-9]{3}[0-9_]$/, 'i'),
@@ -8,13 +8,13 @@ export const phoneRule = () => ({
 });
 export const usernameRule = () => ({
   pattern: new RegExp(/^[0-9a-zA-Z_]*$/),
-  message: 'does not match ^[0-9a-zA-Z]*$',
+  message: 'กรุณาใช้ a-z, A-Z และเลข 0-9 เท่านั้น',
 });
 export const passwordRule = () => ({
   pattern: new RegExp(/.{8,}/),
-  message: 'len must >= 8',
+  message: 'รหัสผ่านต้องมีความยาวอย่างน้อย 8 อักขระ',
 });
 export const emailRule = () => ({
   pattern: new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/),
-  message: 'not email',
+  message: 'email ไม่ถูกต้อง',
 });
