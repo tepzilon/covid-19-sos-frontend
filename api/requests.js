430f5cdb5121fb38aea466a13f9203b2057a8273
Chavin Chuangchaichatchavarn <cc6chavin@gmail.com>
2020-04-05 01:26:07 +0700

    feat(api): set up dev environment for no-backend

--- api/requests.js
+++ api/requests.js
@@ -1,10 +1,46 @@
 const axios = require('axios').default;
 
+const second = () => {
+  return new Promise((resolve) => setTimeout(resolve, 1000));
+};
+
 export const getRequests = async () => {
   try {
-    const data = await axios.get(`${process.env.DOMAIN}/face-shield-request`);
-    return data;
-  } catch (error) {
-    return { error };
-  }
+    await second();
+    return {
+      data: [
+        {
+          _id: '5e88b58374c77a00284eef0d',
+          name: 'bruh',
+          faceShieldDemand: 8000,
+          notice: '',
+          phoneNumber: '09-1231-1111',
+          email: 'mail@test.com',
+          __v: 0,
+          hospitalName: 'hospitalName0',
+          hospitalType: 'private',
+          address: 'addre addersss dsdsd',
+          province: 'ขอนแก่น',
+        },
+        {
+          _id: '5e88cf3b492dbd0028f80abe',
+          name: 'hellochamp',
+          faceShieldDemand: 500,
+          notice: 'bring frech fries',
+          phoneNumber: '09-1231-7711',
+          email: 'mail@est2.com',
+          __v: 0,
+          hospitalName: 'hospitalName22',
+          hospitalType: 'private',
+          address: 'addre addersss dsdsd',
+          province: 'กระบี่',
+        },
+      ],
+      status: 200,
+      statusText: 'OK',
+      headers: {},
+      config: {},
+      request: {},
+    };
+  } catch (error) {}
 };
