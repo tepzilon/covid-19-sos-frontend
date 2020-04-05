025dbf753d16ea6381a60451b8d81a2fa828e085
Chavin Chuangchaichatchavarn <cc6chavin@gmail.com>
2020-04-05 16:31:01 +0700

    feat(register): handle username conflict

--- api/register.js
+++ api/register.js
@@ -11,3 +11,18 @@ export const postRequest = async (requestDatas) => {
     return { error };
   }
 };
+
+export const handlePostRequestError = (error) => {
+  switch (error.response.status) {
+    case 409:
+      return {
+        header: 'Username นี้ถูกใช้งานแล้ว',
+        body: 'กรุณาเลือก Username ใหม่',
+      };
+    default:
+      return {
+        header: 'เซิร์ฟเวอร์ขัดข้อง',
+        body: 'ไม่สามารถทำรายการได้ในขณะนี้',
+      };
+  }
+};
